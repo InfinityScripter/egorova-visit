54fb2ba435ad9f8eb1864e62cffc6b32a7ebc50c
kot <talalaev.misha@gmail.com>
2023-05-26 12:19:25 +0400

    add send form php

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is ready to use project for creating a simple web site. With send form on email.
+just clone repo and replace your data in html and php files.
--- send_form.php
+++ send_form.php
@@ -1,24 +1,44 @@
 <?php
-// Получение данных из формы
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Формирование заголовка письма
-$to = 'e.egorova@ninevia21vek.ru'; // Замените на нужный адрес электронной почты получателя
-$subject = 'Новое сообщение с формы';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
 
-// Формирование тела письма
-$body = "От: $name\n";
-$body .= "Email: $email\n";
-$body .= "Сообщение:\n$message";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-// Отправка письма
-if (mail($to, $subject, $body)) {
-    // Письмо успешно отправлено
-    echo '<p>Спасибо! Ваше сообщение было успешно отправлено.</p>';
-} else {
-    // Произошла ошибка при отправке письма
-    echo '<p>Извините, произошла ошибка. Пожалуйста, повторите попытку позже.</p>';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0; // Отключаем отладочную информацию (0 или false)
+    $mail->CharSet = 'UTF-8'; // Устанавливаем кодировку
+    $mail->Encoding = 'base64'; // Устанавливаем кодировку контента
+    $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+    $mail->Port = 587; // TLS only
+    $mail->SMTPSecure = 'tls'; // ssl is deprecated
+    $mail->SMTPAuth = true;
+    $mail->Username = 'your email'; // email address from which you want to send email
+    $mail->Password = 'app password'; // password
+    $mail->setFrom('your email', 'Zakaz'); // From email and name shod be same as SMTP credentials
+    $mail->addAddress('email add adress', 'name'); // to email and name
+    $mail->Subject = '=?UTF-8?B?' . base64_encode('Заказ') . '?='; // subject в кодировке UTF-8
+    $mail->Body = "Имя: $name\nEmail: $email\nСообщение:\n$message";
+
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "Message sent!";
+    }
 }
 ?>
